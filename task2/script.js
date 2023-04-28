9df2a6eec7fe912da5991522eb070281ba3c538c
VitaliHD <122957465+VitaliHD@users.noreply.github.com>
2023-04-28 21:56:07 +0500

    Add files via upload

---
+++ task2/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn');
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+btn.addEventListener('click', () => {
+    alert('Размер экрана' + screenWidth + ' x ' + screenHeight)
+})
